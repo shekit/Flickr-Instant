9344cbeba2bfc6dae04db1c9e3f1a4df1cd365b3
Abhishek Singh <abhishek@128-122-6-145.dynapool.nyu.edu>
2015-07-18 19:45:20 -0400

    real time instant flickr search

---
+++ config/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export METEOR_SETTINGS="$(cat config/settings.json)"
---
+++ flickr_instant.js
@@ -0,0 +1,92 @@
+if (Meteor.isClient) {
+
+	var commons = true;
+
+	Template.flickr.onRendered(function(){
+		$("#search").focus();
+	})
+
+  	Template.flickr.events({
+  		
+  		'click .checkbox': function(event){
+  			event.preventDefault();
+  			var img = $(".cc");
+  			if(img.hasClass("on")){
+  				img.removeClass("on");
+  				img.addClass("off");
+  				img.attr("src","/cc-off.png");
+  				commons = false;
+  			} else {
+  				img.removeClass("off");
+  				img.addClass("on");
+  				img.attr("src","/cc-on.png");
+  				commons = true;
+  			}
+  			$("#search").focus();
+  		},
+
+	  	'keyup #search':function(event){
+	  		var query = event.target.value.trim();
+	  		query = query.replace(/\s/g,"+");
+
+	  		if(query){
+	  			console.log(query);
+	  			console.log(commons);
+	  			Meteor.call('flickrInstant', query, commons, function(err, res){
+	  				//console.log(err);
+	  				// console.log(res);
+	  				if(res){
+	  					$("#image").attr("src", res);
+	  				} else {
+	  					$("#image").attr("src", "/no_image.png");
+	  				}
+	  				
+	  			})
+	  		} else {
+	  			$("#image").attr("src", "/placeholder.png");
+	  		}
+	  	}
+
+  	})
+}
+
+if (Meteor.isServer) {	
+
+	var key = Meteor.settings.flickrKey;
+
+	var apiCall = function(apiUrl, callback) {
+
+		try {
+			var response = JSON.parse(HTTP.get(apiUrl).content);
+			response = response["photos"]["photo"][0];
+			response = "https://farm"+response["farm"]+".staticflickr.com/"+response["server"]+"/"+response["id"]+"_"+response["secret"]+".jpg";
+			callback(null, response);
+		} catch (error) {
+			var myError = new Meteor.Error(500, 'Faced some problem');
+			callback(myError, null);
+		}
+	}
+
+	Meteor.methods({
+		
+
+		'flickrInstant': function(query, commons){
+			this.unblock();
+			var apiUrl = "";
+			// doesnt work if you change is_commons in url to false
+			if(commons){
+				apiUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+"&text="+query+"&sort=interestingness-desc&privacy_filter=1&content_type=1&media=photos&is_commons=true&per_page=1&format=json&nojsoncallback=1"
+			} else {
+				apiUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+"&text="+query+"&sort=interestingness-desc&privacy_filter=1&content_type=1&media=photos&per_page=1&format=json&nojsoncallback=1"
+			}
+
+			//var apiUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+"&text="+query+"&sort=interestingness-desc&privacy_filter=1&content_type=1&media=photos&is_commons="+is_commons+"&per_page=1&format=json&nojsoncallback=1"
+			console.log(apiUrl);
+			var response = Meteor.wrapAsync(apiCall)(apiUrl);
+			
+			//response = "https://www.flickr.com/photos/"+response["owner"]+"/"+response["id"];
+			return response;
+		}
+	})
+}
+
